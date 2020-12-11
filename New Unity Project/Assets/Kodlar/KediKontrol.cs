e3e6a517b3353dc89de3fbaef34530ce2a7a0a44
Kagan Ayten <kaan.aytn@hotmail.com>
2020-12-11 22:03:39 +0300

    Temel Karakter Kontrolü ve Sahne Dizaynı

---
+++ New Unity Project/Assets/Kodlar/KediKontrol.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KediKontrol : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0,20)]
+    private float hiz;
+    [SerializeField]
+    [Range(0, 20)]
+    private float ziplamaHiz;
+
+    float horizontal;
+    float hareket;
+    Vector2 vek;
+    Animator anim;
+    Rigidbody2D rb;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        hareket += horizontal * hiz * Time.deltaTime;
+        vek = new Vector2(hareket, transform.position.y);
+        transform.position = vek;
+        if (Input.GetButton("Jump"))
+        {
+            KarakterZipla();
+        }
+
+        if (horizontal < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (horizontal > 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (horizontal != 0)
+        {
+            anim.SetBool("yuruyorMu", true);
+        }
+        else
+        {
+            anim.SetBool("yuruyorMu", false);
+        }
+
+    }
+    void KarakterZipla()
+    {
+        rb.AddForce(Vector2.up*ziplamaHiz);
+    }
+}
