c92edc946fc2dc0c48792fcfe256fea3af5926ca
Kagan Ayten <kaan.aytn@hotmail.com>
2020-12-15 21:40:22 +0300

    Kedi Zıplama Düzeltildi

--- New Unity Project/Assets/Kodlar/KediKontrol.cs
+++ New Unity Project/Assets/Kodlar/KediKontrol.cs
@@ -11,26 +11,36 @@ public class KediKontrol : MonoBehaviour
     [Range(0, 20)]
     private float ziplamaHiz;
 
+
     float horizontal;
     float hareket;
     Vector2 vek;
     Animator anim;
     Rigidbody2D rb;
+    public bool yerdeMi;
+    public LayerMask zeminLayer;
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
-
+    
     void Update()
     {
+        yerdeMi = isYerdemi();
+
         horizontal = Input.GetAxis("Horizontal");
         hareket += horizontal * hiz * Time.deltaTime;
         vek = new Vector2(hareket, transform.position.y);
         transform.position = vek;
+
         if (Input.GetButton("Jump"))
         {
-            KarakterZipla();
+            if (isYerdemi()==true)
+            {
+                KarakterZipla();
+            }
+            
         }
 
         if (horizontal < 0)
@@ -51,6 +61,18 @@ public class KediKontrol : MonoBehaviour
             anim.SetBool("yuruyorMu", false);
         }
 
+    }
+    bool isYerdemi()
+    {
+        Vector2 karakterPozisyon = transform.position;
+        Vector2 yon = Vector2.down;
+        float distance = 0.5f;
+        RaycastHit2D hit = Physics2D.Raycast(karakterPozisyon, yon, distance,zeminLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
     }
     void KarakterZipla()
     {
